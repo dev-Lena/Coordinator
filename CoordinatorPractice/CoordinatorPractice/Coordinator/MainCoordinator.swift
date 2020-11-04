058e67db59e2e0175ac0cc4b9fedaff18d8e214c
dev-Lena <keunnalee@gmail.com>
2020-11-04 18:25:37 +0900

    feat: implement Main Coordinator
    #1

---
+++ CoordinatorPractice/CoordinatorPractice/Coordinator/MainCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  MainCoordinator.swift
+//  CoordinatorPractice
+//
+//  Created by Keunna Lee on 2020/11/04.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func buySubscription() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
