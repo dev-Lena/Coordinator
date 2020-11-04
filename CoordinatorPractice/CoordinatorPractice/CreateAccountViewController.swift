9e8eb574bedc8fa9e8ac34847a60ff0040c83b8a
dev-Lena <keunnalee@gmail.com>
2020-11-04 18:32:56 +0900

    feat: create CreateAccountViewController
    #1

---
+++ CoordinatorPractice/CoordinatorPractice/CreateAccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountViewController.swift
+//  CoordinatorPractice
+//
+//  Created by Keunna Lee on 2020/11/04.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController, Storyboarded {
+
+    weak var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
