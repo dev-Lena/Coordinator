1ac7842de6159ee953cc1d5bced96f6c83071a07
dev-Lena <keunnalee@gmail.com>
2020-11-04 18:32:41 +0900

    feat: create BuyViewController
    #1

---
+++ CoordinatorPractice/CoordinatorPractice/BuyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BuyViewController.swift
+//  CoordinatorPractice
+//
+//  Created by Keunna Lee on 2020/11/04.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController, Storyboarded {
+
+    weak var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
