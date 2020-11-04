49c441c961c2f4717d0780b2b2209a5b37aa5195
dev-Lena <keunnalee@gmail.com>
2020-11-04 18:24:53 +0900

    feat: create new project
    #1

---
+++ CoordinatorPractice/CoordinatorPractice/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  CoordinatorPractice
+//
+//  Created by Keunna Lee on 2020/11/04.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
+
